7b6d29ea294f08e61775ef12de3106c56cff3af3
Anatolii <kabantolyan@gmail.com>
2023-05-18 17:50:23 +0300

    basic element reset

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Home page</h1>
+  <ElButton size="large" type="default" round>my btn</ElButton>
+  <ElButton size="default" type="default" round>my btn</ElButton>
+  <ElButton size="small" type="default" round>my btn</ElButton>
+  <ElButton size="large" type="primary" round>my btn</ElButton>
+  <ElButton size="default" type="primary" round>my btn</ElButton>
+  <ElButton size="small" type="primary" round>my btn</ElButton>
+  <ElInput placeholder="placeholder" round />
+  <ElCheckbox />
 </template>
