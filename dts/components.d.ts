672d05d720f916e7fdb8dffd21a0f51f56c98b1e
Anatolii <kabantolyan@gmail.com>
2023-05-19 12:38:26 +0300

    FR-2: update-ui-kit

--- dts/components.d.ts
+++ dts/components.d.ts
@@ -9,21 +9,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElCard: typeof import('element-plus/es')['ElCard']
-    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
-    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-  }
-  export interface ComponentCustomProperties {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,3 @@
 <template>
-  <h1>Home page</h1>
-  <ElButton size="large" type="default" round>my btn</ElButton>
-  <ElButton size="default" type="default" round>my btn</ElButton>
-  <ElButton size="small" type="default" round>my btn</ElButton>
-  <ElButton size="large" type="primary" round>my btn</ElButton>
-  <ElButton size="default" type="primary" round>my btn</ElButton>
-  <ElButton size="small" type="primary" round>my btn</ElButton>
-  <ElInput placeholder="placeholder" round />
-  <ElCheckbox />
+  <h1>main</h1>
 </template>
