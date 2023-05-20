fecfa82b1f27a0602bc244dc77d4275ffdfdf173
Anatolii <49586997+TolyaDeveloper@users.noreply.github.com>
2023-05-20 11:45:41 +0300

    FR-2: UI kit (#3)
    * Add tailwind colors and default layout
    
    * basic element reset
    
    * FR-2: update-ui-kit

--- tailwind/tailwind.colors.cjs
+++ tailwind/tailwind.colors.cjs
@@ -1,5 +1,30 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  ...colors
+  ...colors,
+
+  primary: {
+    100: '#f9f9f9',
+    200: '#f5f5f5',
+    300: '#ebebeb',
+    400: '#d1d1d1',
+    500: '#a9a9a9',
+    600: '#575757',
+    700: '#151515'
+  },
+  accent: {
+    100: '#f4f8ec',
+    200: '#c8deb3',
+    300: '#92c064',
+    400: '#6a983c',
+    500: '#46760a'
+  },
+  secondary: {
+    100: '#fff1ed',
+    200: '#f7c6b7',
+    300: '#eb8d70',
+    400: '#e6704b',
+    500: '#c7522d'
+  },
+  'app-background': '#f9f9f9'
 }
